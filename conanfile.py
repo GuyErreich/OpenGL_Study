f99312b7caf40c228deaf3bde54d2a486f36a61e
Guy Erreich <guyerreich@gmail.com>
2024-06-30 00:45:43 +0300

    Modifications: - My First Window - Configured Conan to work with my OpenGL project

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,7 @@
 import os
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
-from conan.tools.build import build_jobs
-from conan.tools.files import copy, get, mkdir, rmdir
-from conan.tools.system import package_manager
-from conan.tools.system.package_manager import Apt
+from conan.tools.files import copy
 
 class MyOpenGLProject(ConanFile):
     name = "my_opengl_project"
@@ -16,33 +13,26 @@ class MyOpenGLProject(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = {"shared": False}
-    # generators = "CMakeDeps", "CMakeToolchain"
     generators = "CMakeToolchain"
-    exports_sources = "*"
 
     def requirements(self):
-        mkdir(self, 'extensions')
-        get(self, "https://github.com/glfw/glfw/archive/refs/tags/3.4.tar.gz", destination='extensions')
-    #     self.requires("glfw/3.4")
+        self.requires("glfw/3.4")
+        self.requires("opengl/system")
 
-    def system_requirements(self):
-        Apt(self).install(["libwayland-dev", "pkg-config", "libxkbcommon-dev", "xorg-dev"],True,True)
-        # if self.settings.os == "Windows":
-        #     self.build_requires("pkg_config_installer/0.29.2@bincrafters/stable")
+    def build_requirements(self):
+        if self.settings.os != "Windows":
+            self.tool_requires('cmake/3.29.6')
 
     def layout(self):
         cmake_layout(self)
 
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.set_property("opengl", "cmake_file_name", "OpenGL")
+        deps.set_property("opengl", "cmake_target_name", "OpenGL::GL")
+        deps.generate()
+
     def build(self):
         cmake = CMake(self)
-        cmake.configure(build_script_folder="extensions/glfw-3.4")
+        cmake.configure()
         cmake.build(cli_args=["--clean-first"])
-        # cmake.configure()
-        
-
-    def package(self):
-        cmake = CMake(self)
-        cmake.install()
-
-    def package_info(self):
-        self.cpp_info.libs = ["glfw"]
